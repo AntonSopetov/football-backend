09d24472b66dafacdc86c6d465a5aef4c8e1a622
AntonSopetov <greatsurgeon@mail.ru>
2026-06-06 06:02:07 +0400

    cleanup: remove feature files from master

--- src/main/java/com/juno/football/model/EventType.java
+++
@@ -1,8 +0,0 @@
-package com.juno.football.model;
-
-public enum EventType {
-    FOOTBALL_ADULT,  // Взрослый футбол (18+)
-    FOOTBALL_KIDS,   // Детские тренировки Sport Park Juno
-    FIFA_TOURNAMENT, // Турниры на PlayStation
-    TENNIS_PADDLE    // Теннис / Падл
-}
--- src/main/java/com/juno/football/model/RegistrationStatus.java
+++
@@ -1,7 +0,0 @@
-package com.juno.football.model;
-
-public enum RegistrationStatus {
-    ACTIVE,    // Игрок в основном составе
-    WAITLIST   // Игрок в списке ожидания (мест нет)
-}
-
